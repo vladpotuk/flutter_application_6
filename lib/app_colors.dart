026d8c2cfef41a132041da6e38c8dc5274698765
vladpotuk <vladpotuk01@gmail.com>
2024-03-06 21:43:09 +0200

    first commit

---
+++ lib/app_colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color backgroundColor = Colors.white;
+  static const Color darkBackgroundColor = Colors.black;
+  static const Color textHeadlineColor = Colors.black;
+  static const Color textTitleColor = Colors.black87;
+  static const Color textLableColor = Colors.grey;
+  static const Color textBodyColor = Colors.black54;
+  static const Color darkTextHeadlineColor = Colors.white;
+  static const Color darkTextTitleColor = Colors.white70;
+  static const Color darkTextLableColor = Colors.grey;
+  static const Color darkTextBodyColor = Colors.white60;
+  static const Color btnGradientEnd = Colors.red; // Приклад кольору для градієнту
+}
---
+++ lib/main.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_4/app_colors.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+
+class AppTheme {
+  static final light = ThemeData(
+    iconTheme: IconThemeData(
+      color: AppColors.textHeadlineColor,
+    ),
+    appBarTheme: AppBarTheme(
+      backgroundColor: AppColors.backgroundColor,
+      elevation: 0,
+      iconTheme: IconThemeData(
+        color: AppColors.textHeadlineColor,
+      ),
+    ),
+    cardColor: AppColors.backgroundColor,
+    cardTheme: CardTheme(
+      shadowColor: AppColors.darkBackgroundColor,
+      color: AppColors.backgroundColor,
+      surfaceTintColor: AppColors.backgroundColor,
+      elevation: 10,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20),
+        side: BorderSide(
+          color: Colors.blue, // Додано обводку зі синього кольору
+          width: 2.0,
+        ),
+      ),
+    ),
+    primarySwatch: Colors.blue,
+    scaffoldBackgroundColor: AppColors.backgroundColor,
+    textTheme: TextTheme(
+      headlineMedium: TextStyle(
+        color: AppColors.textHeadlineColor,
+        fontSize: 144,
+        fontWeight: FontWeight.bold,
+        fontFamily: GoogleFonts.poppins().fontFamily,
+      ),
+      titleMedium: TextStyle(
+        color: AppColors.textTitleColor,
+        fontSize: 40,
+        fontWeight: FontWeight.bold,
+        fontFamily: GoogleFonts.poppins().fontFamily,
+      ),
+      labelMedium: TextStyle(
+        color: AppColors.textLableColor,
+        fontSize: 24,
+        fontWeight: FontWeight.bold,
+        fontFamily: GoogleFonts.roboto().fontFamily,
+      ),
+      bodyMedium: TextStyle(
+        color: AppColors.textBodyColor,
+        fontSize: 14,
+        fontWeight: FontWeight.bold,
+        fontFamily: GoogleFonts.roboto().fontFamily,
+      ),
+    ),
+  );
+
+  static final dark = ThemeData(
+    iconTheme: IconThemeData(
+      color: AppColors.darkTextHeadlineColor,
+    ),
+    appBarTheme: AppBarTheme(
+      backgroundColor: AppColors.darkBackgroundColor,
+      elevation: 0,
+      iconTheme: IconThemeData(
+        color: AppColors.darkTextHeadlineColor,
+      ),
+    ),
+    cardColor: AppColors.darkBackgroundColor,
+    cardTheme: CardTheme(
+      shadowColor: AppColors.btnGradientEnd,
+      color: AppColors.darkBackgroundColor,
+      surfaceTintColor: AppColors.darkBackgroundColor,
+      elevation: 10,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20),
+        side: BorderSide(
+          color: Colors.red, // Додано обводку з червоного кольору
+          width: 2.0,
+        ),
+      ),
+    ),
+    primarySwatch: Colors.blue,
+    scaffoldBackgroundColor: AppColors.darkBackgroundColor,
+    textTheme: TextTheme(
+      headlineMedium: TextStyle(
+        color: AppColors.darkTextHeadlineColor,
+        fontSize: 144,
+        fontWeight: FontWeight.bold,
+        fontFamily: GoogleFonts.poppins().fontFamily,
+      ),
+      titleMedium: TextStyle(
+        color: AppColors.darkTextTitleColor,
+        fontSize: 40,
+        fontWeight: FontWeight.bold,
+        fontFamily: GoogleFonts.poppins().fontFamily,
+      ),
+      labelMedium: TextStyle(
+        color: AppColors.darkTextLableColor,
+        fontSize: 24,
+        fontWeight: FontWeight.bold,
+        fontFamily: GoogleFonts.roboto().fontFamily,
+      ),
+      bodyMedium: TextStyle(
+        color: AppColors.darkTextBodyColor,
+        fontSize: 14,
+        fontWeight: FontWeight.bold,
+        fontFamily: GoogleFonts.roboto().fontFamily,
+      ),
+    ),
+  );
+}
